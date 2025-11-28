716872410d6d49de655c30b6c622310bb240ddc8
Quarantined Cheese <ethanpothier2@gmail.com>
2025-11-28 14:28:13 -0500

    readability and fixed month detection

--- js/calendar.js
+++ js/calendar.js
@@ -1,35 +1,36 @@
-const today = new Date();
-const month = today.getMonth()
-const year = today.getFullYear()
-const firstDay = new Date(year, month, 1);
-const dayWeek = firstDay.getDay();
-const daysInMonth = new Date(year, month+1, 0).getDate()
+const today = new Date(); // Gets current day
+const month = today.getMonth() // Based on the day, gets the month
+const year = today.getFullYear() // Based on the day, gets the year
+const firstDay = new Date(year, month, 1); //Gets first day of the month
+const dayWeek = firstDay.getDay(); // Gets the day of the week
+const daysInMonth = new Date(year, month+1, 0).getDate() // i'll be honest, idk
 
 
 const monthDisp = document.getElementById("Month")
-switch(month+1) {
-    case 0: monthDisp.innerText = "January" // Jan
-    case 1: monthDisp.innerText = "Feburary" // Feburary
-    case 2: monthDisp.innerText = "March" // March
-    case 3: monthDisp.innerText = "April" // April
-    case 4: monthDisp.innerText = "May" // May
-    case 5: monthDisp.innerText = "June" // June
-    case 6: monthDisp.innerText = "July" // July
-    case 7: monthDisp.innerText = "August" // August
-    case 8: monthDisp.innerText = "September" // September
-    case 9: monthDisp.innerText = "October" // October
-    case 10: monthDisp.innerText = "November" // November
-    case 11: monthDisp.innerText = "December" // December
+console.log(month)
+switch(month) {
+    case 0: monthDisp.innerText = "January"; break; // Jan
+    case 1: monthDisp.innerText = "Feburary"; break; // Feburary
+    case 2: monthDisp.innerText = "March"; break; // March
+    case 3: monthDisp.innerText = "April"; break; // April
+    case 4: monthDisp.innerText = "May"; break; // May
+    case 5: monthDisp.innerText = "June"; break; // June
+    case 6: monthDisp.innerText = "July"; break; // July
+    case 7: monthDisp.innerText = "August"; break; // August
+    case 8: monthDisp.innerText = "September"; break; // September
+    case 9: monthDisp.innerText = "October"; break; // October
+    case 10: monthDisp.innerText = "November"; break; // November
+    case 11: monthDisp.innerText = "December"; break; // December
 }
 monthDisp.innerText += " " + year;
 
 const calendarDays = document.getElementsByClassName("calendar-day");
 for (let i = 0; i < calendarDays.length; i++) {
     const day = calendarDays[i];
-    if (i < dayWeek) { day.innerHTML = ""
+    if (i < dayWeek) { day.innerHTML = "";
 
     } else if (i-dayWeek < daysInMonth){
-        day.innerHTML = ((i-dayWeek)+1) + "<button class='add-event-button'></button>"
+        day.innerHTML = ((i-dayWeek)+1) + "<button class='add-event-button'></button>";
     } else {day.innerHTML = ""}
    
 }
